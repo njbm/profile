ce358833d6ecbd35573f29113f131ce679d04a8b
Jaber Masud <njb99m@gmail.com>
2025-08-16 21:28:45 +0600

    added projects & updated index

--- assets/js/project.js
+++ assets/js/project.js
@@ -8,13 +8,40 @@ const projects = [
         link: "https://codecanyon.net/item/waiz-digital-wallet-and-remittance-app-and-website-with-admin-panel/53238316",
         galleryTitle: "Waiz - Global Money Transfer Solution"
     },
-
     {
         title: "Pay Secure - A Complete Digital Wallet Solution",
         image: "assets/img/portfolio/pay-secure.png",
         category: "filter-web",
         link: "https://codecanyon.net/item/pay-secure-a-complete-payment-processor-solution/41406578",
         galleryTitle: "Pay Secure - A Complete Digital Wallet Solution"
+    },
+    {
+        title: "Merchant Panel Add-on For Pay Secure Wallet",
+        image: "assets/img/portfolio/pay-secure-merchant-addon.png",
+        category: "filter-web",
+        link: "https://codecanyon.net/item/merchant-panel-addon-for-pay-secure/58740637",
+        galleryTitle: "Merchant Panel Add-on For Pay Secure Wallet"
+    },
+    {
+        title: "Agent Panel Add-on For Pay Secure Wallet",
+        image: "assets/img/portfolio/pay-secure-agent-addon.png",
+        category: "filter-web",
+        link: "https://codecanyon.net/item/agent-panel-addon-for-pay-secure/58740639",
+        galleryTitle: "Agent Panel Add-on For Pay Secure Wallet"
+    },
+    {
+        title: "Pay Secure - Digital Wallet Application For Agent (Android, iOS)",
+        image: "assets/img/portfolio/pay-secure-agent-app.png",
+        category: "filter-app",
+        link: "https://codecanyon.net/item/pay-secure-digital-wallet-application-for-agent-android-ios/58793829",
+        galleryTitle: "Pay Secure - Digital Wallet Application For Agent (Android, iOS)"
+    },
+    {
+        title: "Pay Secure - Digital Wallet Application For Merchant (Android, iOS)",
+        image: "assets/img/portfolio/pay-secure-merchant-app.png",
+        category: "filter-app",
+        link: "https://codecanyon.net/item/pay-secure-digital-wallet-application-for-merchant-android-ios/58793831",
+        galleryTitle: "Merchant Panel Application For Pay Secure Wallet"
     },
 
     {
